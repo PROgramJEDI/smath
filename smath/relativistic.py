da8d750d7e6e4989087dbe6f489b3d6108e042cf
PROgramJEDI <tamirglobus@gmail.com>
2021-03-24 13:02:36 +0200

    Update relativistic.py

--- smath/relativistic.py
+++ smath/relativistic.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from scipy.optimize import fsolve
-from scipy.integrate import quad
 from scipy.misc import derivative
+from scipy.integrate import cumulative_trapezoid as integral
+
 
 
 def set_axes():
@@ -17,19 +18,38 @@ def set_axes():
 def relfunc(f, rel, x_start=-20, x_end=20, accuracy=10, dx=10**(-3)):
 	# the first derivitive of f(x).
 	f_tag = lambda x: derivative(f, x, dx=dx)
-	f_arc_length = lambda x: np.sqrt(1 + np.square(f_tag(x)))
+	f_arc_length = lambda x: np.sqrt(1 + f_tag(x) ** 2)
 
-	a_range = np.linspace(x_start, x_end, (x_end - x_start) * accuracy)
-	x_arc_length = np.array([quad(f_arc_length, 0, x)[0] for x in a_range])
+	# the current system of reference X-axis.
+	a_range = np.linspace(x_start, x_end, int((x_end - x_start) * accuracy))
+	a_range = a_range[a_range != 0]
 
-	# the orthagonal line to the tangent on a specific point.
+	# prepare the two groups to be integrated seperatly.
+	a_negetives, a_positives = a_range[a_range < 0], a_range[a_range > 0] 
+	a_range = np.concatenate((a_negetives, a_positives))
+
+	# the continous integral of the two groups.
+	negetive_x_arc_length = integral(f_arc_length(a_negetives), a_negetives, dx=dx)	
+	positive_x_arc_length = integral(f_arc_length(a_positives), a_positives, dx=dx, initial=0)
+
+	# reduce to the same shape of the axis of the negetives integration.
+	a_range = a_range[1:]
+	# concat the groups into one field. the negitive group should be negitive.
+	x_arc_length = np.concatenate((-negetive_x_arc_length, positive_x_arc_length))
+	
+	# applying the given range for the new reference system.
+	x_arc_range = (x_arc_length >= x_start) & (x_arc_length <= x_end) #***FIX: this can result different shapes for a_range and x_arc_length. can be fixed by recuding one array
+	x_arc_length, a_range = x_arc_length[x_arc_range], a_range[x_arc_range]
+
+	# the orthagonal linear function to the tangent in a specific point.
 	m = lambda x: (-1/f_tag(x_arc_length))*(x-x_arc_length)+f(x_arc_length)
 	solutions_func = lambda x: m(x) - rel(x)
+	
 	# solve rel(x) = m(x).
-	x_solutions = np.asarray(fsolve(solutions_func, x_arc_length))
+	x_solutions = fsolve(solutions_func, x_arc_length)
 
 	y_diff = m(x_solutions) - m(x_arc_length)
 	x_diff = x_solutions - x_arc_length
 
-	distances = np.sqrt(x_diff ** 2 + y_diff ** 2) * (y_diff / abs(y_diff))
-	return zip(x_arc_length, distances)
+	distances = np.sqrt(x_diff ** 2 + y_diff ** 2) * (y_diff / np.absolute(y_diff))
+	return x_arc_length, distances
